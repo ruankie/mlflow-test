e75a663f17f5b9d6fab9cede844e5cc1ec2b097f
root <pretoriusr.7@gmail.com>
2022-05-19 10:08:51 +0000

    updated data

--- src/utils.py
+++ src/utils.py
@@ -4,5 +4,5 @@ import wget
 def download_data(url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"):
     """Download Iris data and save in data/ folder
     """
-    if not os.path.isfile('iris.data'):
-        wget.download(url=url)
+    if not os.path.isfile('../data/iris.csv'):
+        wget.download(url=url, out='../data/iris.csv')
